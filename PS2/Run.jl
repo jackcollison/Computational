28b3cb56eb0d74ad5773ec82fb50782f1cf8c5fe
YG_Yun <55074079+yoonyg5512@users.noreply.github.com>
2022-09-16 17:07:45 -0500

    Run file modified to solve the problems
    Done!

--- PS2/Run.jl
+++ PS2/Run.jl
@@ -14,20 +14,20 @@ JLD2.jldsave("/Users/Yeonggyu/Desktop/Huggett_rep.jld2", prim = prim, res = res)
 
 ## Policy Function
 
-Plots.plot(a_grid, val_func, title="Value Function", label =["Employed" "Unemployed"], legend=:topleft)
+Plots.plot(a_grid, val_func, label =["Employed" "Unemployed"], legend=:topleft)
 Plots.savefig("/Users/Yeonggyu/Desktop/Value Function.png")
 
 a_max_level = a_grid[findall(a_grid .== pol_func[:,1])]
 findall(a_grid .== pol_func[:,2])
 
-Plots.plot(a_grid, pol_func, title="Policy Function", label =["Employed" "Unemployed"], legend=:topleft)
-Plots.plot!(a_grid, a_grid, linestyle=:dash) # 45-degree line
+Plots.plot(a_grid, pol_func, label =["Employed" "Unemployed"], legend=:topleft)
+Plots.plot!(a_grid, a_grid, linestyle=:dash, label="") # 45-degree line
 Plots.savefig("/Users/Yeonggyu/Desktop/Policy Function.png")
 
 ## Wealth Distribution plot
 using DataFrames
 
-Plots.plot(a_grid, res.mu, title = "Asset Distribution", label =["Employed" "Unemployed"], legend=:topleft)
+Plots.plot(a_grid, res.mu, label =["Employed" "Unemployed"], legend=:topleft)
 Plots.savefig("/Users/Yeonggyu/Desktop/Asset Distribution.png")
 
 Em_mu = [a_grid.+Es[1] res.mu[:,1]]
@@ -42,17 +42,31 @@ disallowmissing!(wealth_mat)
 wealth_mat = Matrix(wealth_mat)
 wealth_mat = sortslices(wealth_mat,dims=1,by=x->(x[1],-x[2],-x[3]),rev=false)
 
-Plots.plot(wealth_mat[:,1], wealth_mat[:,2:3], title = "Wealth Distribution", seriestype = :line, label =["Employed" "Unemployed"], legend=:topleft)
+Plots.plot(wealth_mat[:,1], wealth_mat[:,2:3], seriestype = :line, label =["Employed" "Unemployed"], legend=:topleft)
 Plots.savefig("/Users/Yeonggyu/Desktop/Wealth Distribution.png")
+
 ## Lorenz Curve
 
 lorenz_mat = [cumsum(wealth_mat[:,1].*(wealth_mat[:,2].+wealth_mat[:,3])./sum(wealth_mat[:,1].*(wealth_mat[:,2].+wealth_mat[:,3]))) cumsum(wealth_mat[:,2].+wealth_mat[:,3])]
 
-Plots.plot(lorenz_mat[:,2], lorenz_mat[:,1], xlabel="Fraction of Households", ylabel="Fraction of Wealth", label="", title = "Lorenz Curve")
+Plots.plot(lorenz_mat[:,2], lorenz_mat[:,1], xlabel="Fraction of Households", ylabel="Fraction of Wealth", label="")
+Plots.plot!(lorenz_mat[:,2], lorenz_mat[:,2], linestyle=:dash, label="") # 45-degree line
 Plots.savefig("/Users/Yeonggyu/Desktop/Lorenz Curve.png")
 
-gini = 
+ind_1 = findall(lorenz_mat[:,1] .> lorenz_mat[:,2])[2] - 1 # Find the endpoint of Lorenz Curve
+gini = sum(lorenz_mat[1:ind_1,2].-lorenz_mat[1:ind_1,1]) /sum(lorenz_mat[1:ind_1,2])
 
 ## Welfare Comparison
 
-W_fb = 1 / (1 - prim.β) * 0.9717^(1-prim.α) / (1 - prim.α) # Welfare in a complete market economy
+@unpack α, β = prim
+W_fb = 1 / (1 - β) * ((0.9717^(1- α)-1) / (1 -  α)) # Welfare in a complete market economy
+
+lambdas = ((W_fb + 1/((1- α)*(1- β)))./(val_func .+ 1/((1- α)*(1- β)))).^(1/(1- α)) .- 1
+
+Plots.plot(a_grid,lambdas[:,1:2], label = ["Employed" "Unemployed"])
+Plots.savefig("/Users/Yeonggyu/Desktop/Lambdas.png")
+
+W_inc = sum(res.mu.*val_func)
+WG = sum(res.mu.*lambdas)
+
+gainers = sum(res.mu.*(lambdas .>= 0))
