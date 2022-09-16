18b26d33e361d5aa05f5118427a6465eff196b48
YG_Yun <55074079+yoonyg5512@users.noreply.github.com>
2022-09-16 14:21:23 -0500

    Working files
    Replication of Huggett (1993) Done!!!

--- PS2/Function_incomplete.jl
+++ PS2/Function_incomplete.jl
@@ -5,8 +5,8 @@
     Es::Array{Float64} = [1.0, 0.5] #earnings
     a_min::Float64 = -2 #asset lower bound
     a_max::Float64 = 5 #asset upper bound
-    length_a_grid::Int64 = 1000 #number of asset grid points
-    a_grid::Array{Float64,1} = collect(range(a_min, length = length_a_grid, stop = a_max)) #capital grid
+    length_a_grid::Int64 = 500 #number of asset grid points
+    a_grid::Array{Float64,1} = collect(range(a_min, length = length_a_grid, stop = a_max)) #asset grid
     Π::Matrix{Float64} = [0.97 0.5; 0.03 0.5] #transpose of original transition matrix
 end
 
@@ -23,8 +23,8 @@ function Initialize()
     prim = Primitives() #initialize primtiives
     val_func = reshape(zeros(2*prim.length_a_grid), prim.length_a_grid, 2) #initial value function guess
     pol_func = reshape(zeros(2*prim.length_a_grid), prim.length_a_grid, 2) #initial policy function guess
-    q = 0.9932 #initial bond price to guess
-    mu = ones(prim.length_a_grid) ./ prim.length_a_grid * [0.9434 0.0566] # initial guess for mu
+    q = 0.994 #initial bond price to guess
+    mu = ones(prim.length_a_grid) * [0.9434/prim.length_a_grid 0.0566/prim.length_a_grid] # initial guess for mu
     res = Results(val_func, pol_func, q, mu) #initialize results struct
     prim, res #return deliverables
 end
@@ -74,7 +74,7 @@ end
 function get_mu(prim::Primitives, res::Results; tol::Float64 = 1e-4, err::Float64 = 100.0)
     @unpack a_grid, length_a_grid, Π, β = prim
     ED = 0.01 # excess demand
-    mu_new = zeros(length_a_grid, 2)
+
         println("###############################################")
         println("######## SOLVING DISTRIBUTION PROBLEM #########")
         println("###############################################\n")
@@ -83,9 +83,11 @@ function get_mu(prim::Primitives, res::Results; tol::Float64 = 1e-4, err::Float6
         err = 100.0
         get_g(prim, res)
         while err>tol
+            mu_new = zeros(length_a_grid, 2)
+
             for i = 1:length_a_grid, j = 1:2
                 a_new = a_grid[i] # a'
-                mu_new[i,j] = sum(res.mu.*(res.pol_func .== a_new) * Π[j,:])
+                mu_new[i,j] = sum(res.mu.*(res.pol_func .== a_new) * Π'[:,j])
             end
             err = maximum(abs.((mu_new .- res.mu)./res.mu))
             res.mu = mu_new
@@ -95,14 +97,14 @@ function get_mu(prim::Primitives, res::Results; tol::Float64 = 1e-4, err::Float6
 
         if ED > tol
             q_old = res.q
-            res.q = res.q + 0.01 * (1 - q_old)/2
+            res.q = res.q + abs(ED)/10 * (1 - q_old)/2
 
             println("\n******************************************************************\n")
             @printf "Excess Demand = %-8.6g New Price = %.6f\n\n" ED res.q
             println("******************************************************************\n")
         elseif ED < -tol
             q_old = res.q
-            res.q = res.q - 0.01 * (1 - q_old)/2
+            res.q = res.q - abs(ED)/10 * (q_old - β)/2
 
             println("\n******************************************************************\n")
             @printf "Excess Demand = %-8.6g New Price = %.6f\n\n" ED res.q
