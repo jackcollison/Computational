e890fcca5d704b48211ad6271427e45732b55ec3
Jack Collison <jcollison97@gmail.com>
2022-09-18 20:28:40 -0500

    Update PS1 files
    - Update parallelization

--- PS1/02Growth_compute.jl
+++ PS1/02Growth_compute.jl
@@ -1,24 +1,27 @@
-using Parameters, Plots #import the libraries we want
-include("02Growth_model.jl") #import the functions that solve our growth model
+using Distributed
+addprocs(3)
 
-prim, res = Initialize() #initialize primitive and results structs
+@everywhere using Parameters, Plots #import the libraries we want
+@everywhere include("02Growth_model.jl") #import the functions that solve our growth model
+
+@everywhere prim, res = Initialize() #initialize primitive and results structs
 @elapsed Solve_model(prim, res) #solve the model!
 @unpack val_func, pol_func = res
 @unpack k_grid = prim
 
 ##############Make plots
 #value function
-Plots.plot(k_grid, val_func, title="Value Function")
-# Plots.savefig("02_Value_Functions.png")
+Plots.plot(k_grid, val_func, title="Value Function", label=["High Productivity" "Low Productivity"])
+#Plots.savefig("02_Value_Functions.png")
 
 #policy functions
-Plots.plot(k_grid, pol_func, title="Policy Functions")
-# Plots.savefig("02_Policy_Functions.png")
+Plots.plot(k_grid, pol_func, title="Policy Functions", label=["High Productivity" "Low Productivity"])
+#Plots.savefig("02_Policy_Functions.png")
 
 #changes in policy function
 pol_func_δ = copy(pol_func).-k_grid
-Plots.plot(k_grid, pol_func_δ, title="Policy Functions Changes")
-# Plots.savefig("02_Policy_Functions_Changes.png")
+Plots.plot(k_grid, pol_func_δ, title="Policy Functions Changes", label=["High Productivity" "Low Productivity"])
+#Plots.savefig("02_Policy_Functions_Changes.png")
 
 println("All done!")
 ################################
--- PS1/02Growth_model.jl
+++ PS1/02Growth_model.jl
@@ -1,76 +1,74 @@
+using Distributed
+@everywhere using LinearAlgebra, SharedArrays
+
 #keyword-enabled structure to hold model primitives
-@with_kw struct Primitives
+@everywhere @with_kw struct Primitives
     β::Float64 = 0.99 #discount rate
     δ::Float64 = 0.025 #depreciation rate
     α::Float64 = 0.36 #capital share
     k_min::Float64 = 0.01 #capital lower bound
-    k_max::Float64 = 75.0 #capital upper bound
+    k_max::Float64 = 45.0 #capital upper bound
     nk::Int64 = 1000 #number of capital grid points
     k_grid::Array{Float64,1} = collect(range(k_min, length = nk, stop = k_max)) #capital grid
-    Π::Matrix{Float64} = [0.977 0.074; 0.023 0.926] #transpose of original transition matrix
-    Z::Vector{Float64} = [1.25, 0.2]
+    markov::Array{Float64,2} = [0.977 0.023; 0.074 0.926]
+    z_grid::Array{Float64,1} = [1.25, 0.2]
+    nz::Int64 = length(z_grid)
 end
 
 #structure that holds model results
-mutable struct Results
-    val_func::Matrix{Float64} #value function
-    pol_func::Matrix{Float64} #policy function
+@everywhere mutable struct Results
+    val_func::SharedArray{Float64, 2} #value function
+    pol_func::SharedArray{Float64, 2} #policy function
 end
 
 #function for initializing model primitives and results
-function Initialize()
+@everywhere function Initialize()
     prim = Primitives() #initialize primtiives
-    val_func = reshape(zeros(2*prim.nk), prim.nk, 2) #initial value function guess
-    pol_func = reshape(zeros(2*prim.nk), prim.nk, 2) #initial policy function guess
+    val_func = SharedArray{Float64}(zeros(prim.nk, prim.nz)) #initial value function guess
+    pol_func = SharedArray{Float64}(zeros(prim.nk, prim.nz)) #initial policy function guess
     res = Results(val_func, pol_func) #initialize results struct
     prim, res #return deliverables
 end
 
 #Bellman Operator
-function Bellman(prim::Primitives,res::Results)
-    @unpack val_func = res #unpack value function
-    @unpack k_grid, β, δ, α, nk, Π, Z = prim #unpack model primitives
-    v_next = reshape(zeros(2*nk), nk, 2) #next guess of value function to fill
-    c = zeros(2) #consumption matrix to fill
+@everywhere function Bellman(prim::Primitives,res::Results)
+    @unpack β, δ, α, k_grid, nk, markov, z_grid, nz = prim #unpack model primitives
+    v_next = SharedArray{Float64}(zeros(nk, nz)) #next guess of value function to fill
 
-    for k_index = 1:nk
+    #@sync @distributed for i = 1:nz*nk
+    @sync @distributed for i = 1:nz*nk
+        z_index = mod(i, 2) + 1
+        k_index = mod(ceil(Int64, i / nz), nk) + 1
+        z = z_grid[z_index]
         k = k_grid[k_index] #value of k
-        candidate_max1 = -Inf #bad candidate max
-        candidate_max2 = -Inf #bad candidate max
-
-        budget1 = Z[1]*k^α + (1-δ)*k #budget
-        budget2 = Z[2]*k^α + (1-δ)*k #budget
+            
+        candidate_max = -Inf #bad candidate max
+        budget = z * k^α + (1-δ)*k #budget
 
         for kp_index in 1:nk #loop over possible selections of k', exploiting monotonicity of policy function
-            c[1] = budget1 - k_grid[kp_index] #consumption given k' selection
-            c[2] = budget2 - k_grid[kp_index]
-            if c[1]>0 #check for positivity
-                val1 = log(c[1]) + β*val_func[kp_index,:]'Π[:,1] #compute value
-                if val1>candidate_max1 #check for new max value
-                    candidate_max1 = val1 #update max value
-                    res.pol_func[k_index,1] = k_grid[kp_index] #update policy function
-                end
-            end
-            if c[2]>0 #check for positivity
-                val2 = log(c[2]) + β*val_func[kp_index,:]'Π[:,2] #compute value
-                if val2>candidate_max2 #check for new max value
-                    candidate_max2 = val2 #update max value
-                    res.pol_func[k_index,2] = k_grid[kp_index] #update policy function
+            c = budget - k_grid[kp_index] #consumption given k' selection
+            if c>0 #check for positivity
+                val = log(c) + β * (res.val_func[kp_index, 1] * markov[z_index, 1] + res.val_func[kp_index, 2] * markov[z_index, 2]) # (res.val_func[kp_index,:] ⋅ markov[z_index,:]) #compute value
+                if val>candidate_max #check for new max value
+                    candidate_max = val #update max value
+                    res.pol_func[k_index, z_index] = k_grid[kp_index] #update policy function
                 end
             end
         end
-        v_next[k_index,:] = [candidate_max1, candidate_max2] #update value function
+        v_next[k_index, z_index] = candidate_max #update value function
     end
     v_next #return next guess of value function
 end
 
 #Value function iteration
-function V_iterate(prim::Primitives, res::Results; tol::Float64 = 1e-4, err::Float64 = 100.0)
+@everywhere function V_iterate(prim::Primitives, res::Results; tol::Float64 = 1e-4, err::Float64 = 100.0)
     n = 0 #counter
 
     while err>tol #begin iteration
         v_next = Bellman(prim, res) #spit out new vectors
-        err = abs.(maximum(v_next.-res.val_func)) #reset error level
+        e1 = abs.(maximum(v_next[:,1].-res.val_func[:,1])) / abs(v_next[prim.nk, 1])
+        e2 = abs.(maximum(v_next[:,2].-res.val_func[:,2])) / abs(v_next[prim.nk, 2])
+        err = max(e1, e2) #reset error level
         res.val_func = v_next #update value function
         n+=1
     end
@@ -78,7 +76,7 @@ function V_iterate(prim::Primitives, res::Results; tol::Float64 = 1e-4, err::Flo
 end
 
 #solve the model
-function Solve_model(prim::Primitives, res::Results)
+@everywhere function Solve_model(prim::Primitives, res::Results)
     V_iterate(prim, res) #in this case, all we have to do is the value function iteration!
 end
 ##############################################################################
