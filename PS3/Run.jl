93bef02a5dfb50246f74498285fd882b9df8e948
YG_Yun <55074079+yoonyg5512@users.noreply.github.com>
2022-09-21 17:15:55 -0500

    Add files via upload

--- PS3/Run.jl
+++ PS3/Run.jl
@@ -3,4 +3,17 @@ using Parameters, Plots, Printf, JLD2, TableView, DelimitedFiles
 include("Function_CK.jl") #import the functions that solve our growth model
 
 prim, res = Initialize()
-#@elapsed Solve_model(prim, res)
+
+## Exercise 1: Value Function Iteration
+
+@elapsed get_g(prim, res)
+Plots.plot(res.val_func_ret[:,5], label="", xlabel ="Asset") # Value function at age 50
+Plots.savefig("/Users/Yeonggyu/Desktop/Econ 899 - Computation/PS/PS3/Value50.png")
+
+Plots.plot(res.pol_func_wor[:,20,:], label = ["Good" "Bad"], xlabel = "Asset", ylabel = "Savings", legend=:topleft) # Savings function at age 20
+Plots.savefig("/Users/Yeonggyu/Desktop/Econ 899 - Computation/PS/PS3/Saving20.png")
+
+## Exercise 2 & 3: Compute Stationary Distribution and Do Counterfactuals
+
+# Initialize again before running
+@elapsed Solve_model(prim, res)
