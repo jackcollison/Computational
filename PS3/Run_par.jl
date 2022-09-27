40b7adbe5527c8dd5f3cd4f8e71b2fd59ff5e7af
YG_Yun <55074079+yoonyg5512@users.noreply.github.com>
2022-09-26 23:28:42 -0500

    Add files via upload

--- PS3/Run_par.jl
+++ PS3/Run_par.jl
@@ -5,10 +5,11 @@ addprocs(4)
 
 @everywhere include("Function_CK_par.jl") #import the functions that solve our growth model
 
-@everywhere prim, res = Initialize()
+@everywhere prim, res = Initialize(θ=0.11, Zs=[3.0, 0.5], γ=0.42) # Baseline model
 
 @time Solve_model(prim, res)
-
+JLD2.jldsave("/Users/Yeonggyu/Desktop/Econ 899 - Computation/PS/PS3/CK_rep.jld2", prim = prim, res = res) # save workspace in case
+#JLD2.@load "/Users/Yeonggyu/Desktop/Econ 899 - Computation/PS/PS3/CK_rep.jld2"
 
 ## Exercise 1: Value Function Iteration
 
@@ -21,7 +22,19 @@ Plots.savefig("/Users/Yeonggyu/Desktop/Econ 899 - Computation/PS/PS3/Saving20.pn
 ## Exercise 2 & 3: Compute Stationary Distribution and Do Counterfactuals
 
 # Without SS: Set θ = 0
+prim_no_ss, res_no_ss = Primitives(θ=0, Zs=[3.0, 0.5], γ=0.42)
+@time Solve_model(prim_no_ss, res_no_ss)
 
 # No risk: Set Zs = [0.5, 0.5]
+prim_no_z, res_no_z = Primitives(θ=0.11, Zs=[0.5, 0.5], γ=0.42)
+@time Solve_model(prim_no_z, res_no_z)
+
+prim_no_z_ss, res_no_z_ss = Primitives(θ=0, Zs=[0.5, 0.5], γ=0.42)
+@time Solve_model(prim_no_z_ss, res_no_z_ss)
 
 # Exogenous labor: Set γ = 1
+prim_ex_lab, res_ex_lab = Primitives(θ=0.11, Zs=[3.0, 0.5], γ=1)
+@time Solve_model(prim_ex_lab, res_ex_lab)
+
+prim_ex_lab_no_ss, res_ex_lab_no_ss = Primitives(θ=0, Zs=[3.0, 0.5], γ=1)
+@time Solve_model(prim_ex_lab_no_ss, res_ex_lab_no_ss)
